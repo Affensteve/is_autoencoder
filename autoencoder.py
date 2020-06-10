28f96f563fb77b17579bd28540e88fe914fc897c
Ante Skoric <ante.skoric@haw-hamburg.de>
2020-06-11 00:49:04 +0200

    Comments

--- autoencoder.py
+++ autoencoder.py
@@ -1,6 +1,5 @@
 """
 MNIST Dataset http://yann.lecun.com/exdb/mnist/
-Created with TensorFlow tutorial
 """
 from __future__ import division, print_function, absolute_import
 
@@ -25,12 +24,12 @@ display_step = 500
 examples_to_show = 10
 
 # Network Parameters
-num_hidden_1 = 256  # 1st layer num features
-num_hidden_2 = 128  # 2nd layer num features (the latent dim)
+num_hidden_1 = 256  # 1st layer (nodes)
+num_hidden_2 = 128  # 2nd layer (nodes)
 # num_hidden_3 = 64
-num_input = 784  # MNIST MNIST_data input (img shape: 28*28)
+num_input = 784  # Data input vector (img shape: 28*28)
 
-# tf Graph input (only pictures)
+# graph input of the pictures
 X = tf.placeholder("float", [None, num_input])
 
 weights = {
@@ -53,10 +52,10 @@ biases = {
 
 # Building the encoder
 def encoder(x):
-    # Encoder Hidden layer with sigmoid activation #1
+    # Hidden layer in the encoder with sigmoid activation 1
     layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']),
                                    biases['encoder_b1']))
-    # Encoder Hidden layer with sigmoid activation #2
+    # Hidden layer in the encoder with sigmoid activation 2
     layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
                                    biases['encoder_b2']))
     return layer_2
@@ -64,51 +63,49 @@ def encoder(x):
 
 # Building the decoder
 def decoder(x):
-    # Decoder Hidden layer with sigmoid activation #1
+    # Hidden layer in the decoder with sigmoid activation 1
     layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),
                                    biases['decoder_b1']))
-    # Decoder Hidden layer with sigmoid activation #2
+    # Hidden layer in the decoder with sigmoid activation 2
     layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
                                    biases['decoder_b2']))
     return layer_2
 
 
-# Construct model
+# Autoencoder model
 encoder_op = encoder(X)
 decoder_op = decoder(encoder_op)
 
 # Prediction
 y_pred = decoder_op
-# Targets (Labels) are the input MNIST_data.
 y_true = X
 
-# Define loss and optimizer, minimize the squared error
+# Loss and optimizer, minimize the squared error
 loss = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
 optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
 
-# Initialize the variables (i.e. assign their default value)
+# Initialize the variables
 init = tf.global_variables_initializer()
 
 # Start Training
-# Start a new TF session
+# Start a a tensorflow session
 with tf.Session() as sess:
-    # Run the initializer
     sess.run(init)
 
     # Training
     for i in range(1, num_steps + 1):
         # Prepare Data
-        # Get the next batch of MNIST MNIST_data (only images are needed, not labels)
+        # Get the next batch of MNIST data only images, not labels
         batch_x, _ = mnist.train.next_batch(batch_size)
 
         # Run optimization op (backprop) and cost op (to get loss value)
         _, l = sess.run([optimizer, loss], feed_dict={X: batch_x})
-        # Display logs per step
+        #Logs
         if i % display_step == 0 or i == 1:
             print('Step %i: Minibatch Loss: %f' % (i, l))
 
     # Testing
-    # Encode and decode images from test set and visualize their reconstruction.
+    # Encode and decode images from test set and visualize them.
     n = 4
     canvas_orig = np.empty((28 * n, 28 * n))
     canvas_recon = np.empty((28 * n, 28 * n))
